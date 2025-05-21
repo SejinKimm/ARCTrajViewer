b550f4de5fdc4f547f70b8fe14ad3c5e8e3c791b
SejinKimm <ksj614@kaist.ac.kr>
2025-05-22 07:05:41 +0900

    test

--- src/ArcTrajViewer.jsx
+++ src/ArcTrajViewer.jsx
@@ -121,7 +121,7 @@ export default function ArcTrajViewer() {
   }, [trajectory]);
 
   return (
-    <div className="flex flex-col min-h-screen w-screen overflow-hidden font-sans">
+    <div className="flex flex-col min-h-screen w-[calc(100vw-1rem)] overflow-hidden font-sans">
       {/* 상단 헤더 */}
       <div className="bg-gray-900 text-white text-center py-6 shadow-md">
         <h1 className="text-4xl font-black mb-2">ARCTraj</h1>
